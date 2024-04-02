a738f46c8c51261727ffec1e431cb0b55e025ee2
anaclarag <gomes06anaclara@gmail.com.br>
2024-04-02 16:21:10 -0300

    criação model categoria

--- core/serializers/acessorio.py
+++ core/serializers/acessorio.py
@@ -2,7 +2,7 @@ from rest_framework.serializers import ModelSerializer
 
 from core.models import Acessorio
 
-class CategoriaSerializer(ModelSerializer):
+class AcessorioSerializer(ModelSerializer):
     class Meta:
         model = Acessorio
         fields = "__all__"
